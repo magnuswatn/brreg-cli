7a6b651186a98cc237ac4ccb2e4de6cfd9472183
Magnus Watn <magnus@watn.no>
2023-02-25 16:46:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+brreg-cli
+====
+
+A small cli tool for looking up Norwegian organization numbers from [Brønnøysundregistrene](https://www.brreg.no/).
+
+```
+$ brreg-cli 983544622
+********************** Organisasjon **********************
+Orgnummer                                   : 983544622
+Navn                                        : HELSEDIREKTORATET
+Forretningsadresse                          : Vitaminveien 4
+                                              0483 OSLO
+Postadresse                                 : Postboks 220 Skøyen
+                                              0213 OSLO
+Hjemmeside                                  : www.helsedirektoratet.no
+Registrert i Enhetsregisteret               : 2001-08-13
+Under avvikling                             : Nei
+Under tvangsavvikling eller tvangsoppløsning: Nei
+Overordnet enhet                            : 983887406 - HELSE- OG OMSORGSDEPARTEMENTET
+Underenheter (20 første)                    : 990543402 - HELSEDIREKTORATET AVD TRONDHEIM
+                                              983593445 - HELSEDIREKTORATET HOVEDKONTOR OSLO V4
+                                              985530904 - PASIENT- OG BRUKAROMBODET I VESTLAND, KONTOR BERGEN
+                                              985531137 - PASIENT- OG BRUKAROMBODET I VESTLAND, KONTOR FØRDE
+                                              985530793 - PASIENT- OG BRUKEROMBUDET I AGDER
+                                              985530262 - PASIENT- OG BRUKEROMBUDET I INNLANDET
+                                              985531161 - PASIENT- OG BRUKEROMBUDET I MØRE OG ROMSDAL
+                                              974621541 - PASIENT- OG BRUKEROMBUDET I NORDLAND
+                                              885530842 - PASIENT- OG BRUKEROMBUDET I ROGALAND
+                                              981479629 - PASIENT- OG BRUKEROMBUDET I TROMS OG FINNMARK, KONTOR HAMMERFEST
+                                              985531269 - PASIENT- OG BRUKEROMBUDET I TROMS OG FINNMARK, KONTOR TROMSØ
+                                              985531188 - PASIENT- OG BRUKEROMBUDET I TRØNDELAG KONTOR TRONDHEIM
+                                              985530726 - PASIENT- OG BRUKEROMBUDET I VESTFOLD OG TELEMARK, KONTOR SKIEN
+                                              985530688 - PASIENT- OG BRUKEROMBUDET I VESTFOLD OG TELEMARK, KONTOR TØNSBERG
+                                              985530327 - PASIENT- OG BRUKEROMBUDET I VIKEN, KONTOR DRAMMEN
+                                              885530222 - PASIENT- OG BRUKEROMBUDET I VIKEN, KONTOR MOSS
+                                              926040901 - SENTER FOR ET ALDERSVENNLIG NORGE
+```
---
+++ src/main.rs
@@ -0,0 +1,450 @@
+use reqwest::{blocking::Client, StatusCode};
+use serde_derive::Deserialize;
+use std::env;
+use std::io::Read;
+use std::time::Duration;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const MAX_TITLE_LENGTH: usize = 44;
+
+// Oh shit, it's på norsk!
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Organization {
+    organisasjonsnummer: String,
+    navn: String,
+    slettedato: Option<String>,
+    registreringsdato_enhetsregisteret: Option<String>,
+    postadresse: Option<Adresse>,
+    forretningsadresse: Option<Adresse>,
+    under_avvikling: Option<bool>,
+    under_tvangsavvikling_eller_tvangsopplosning: Option<bool>,
+    hjemmeside: Option<String>,
+    overordnet_enhet: Option<String>,
+}
+#[derive(Deserialize, Debug)]
+struct Adresse {
+    adresse: Vec<String>,
+    postnummer: Option<String>,
+    poststed: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Underenheter {
+    underenheter: Vec<Organization>,
+}
+
+#[derive(Deserialize, Debug)]
+struct SearchResponse {
+    _embedded: Option<Underenheter>,
+}
+
+// https://data.brreg.no/enhetsregisteret/api/docs/index.html#_500_feil_p%C3%A5_server
+#[derive(Deserialize, Debug)]
+struct BrregInternalServerError {
+    trace: String,
+    error: String,
+    message: String,
+}
+
+#[derive(Debug, PartialEq)]
+enum BrregErrorType {
+    NotFound,
+    Gone,
+    InternalServerError,
+    NetworkError,
+    UnexpectedResponse,
+    JsonParseError,
+}
+
+#[derive(Debug)]
+struct BrregError {
+    typ: BrregErrorType,
+    error: Option<String>,
+}
+
+fn get_organization(client: &Client, orgnr: &str, typ: &str) -> Result<Organization, BrregError> {
+    let http_result = client
+        .get(format!(
+            "https://data.brreg.no/enhetsregisteret/api/{}er/{}",
+            typ, orgnr
+        ))
+        .send();
+
+    if http_result.is_err() {
+        return Err(BrregError {
+            typ: BrregErrorType::NetworkError,
+            error: Some(http_result.unwrap_err().to_string()),
+        });
+    }
+
+    let mut response = http_result.unwrap();
+
+    let mut body = String::new();
+    let read_result = response.read_to_string(&mut body);
+    if read_result.is_err() {
+        return Err(BrregError {
+            typ: BrregErrorType::NetworkError,
+            error: Some(read_result.unwrap_err().to_string()),
+        });
+    }
+
+    match response.status() {
+        StatusCode::OK => {
+            let json_parse_res = serde_json::from_str::<Organization>(&body);
+            if json_parse_res.is_ok() {
+                return Ok(json_parse_res.unwrap());
+            }
+            return Err(BrregError {
+                typ: BrregErrorType::JsonParseError,
+                error: Some(json_parse_res.unwrap_err().to_string()),
+            });
+        }
+
+        StatusCode::NOT_FOUND => Err(BrregError {
+            typ: BrregErrorType::NotFound,
+            error: None,
+        }),
+
+        StatusCode::GONE => Err(BrregError {
+            typ: BrregErrorType::Gone,
+            error: None,
+        }),
+
+        StatusCode::INTERNAL_SERVER_ERROR => {
+            let json_parse_res = serde_json::from_str::<BrregInternalServerError>(&body);
+            if json_parse_res.is_ok() {
+                let brreg_error = json_parse_res.unwrap();
+
+                return Err(BrregError {
+                    typ: BrregErrorType::InternalServerError,
+                    error: Some(format!(
+                        "{}: {} {}",
+                        brreg_error.trace, brreg_error.error, brreg_error.message
+                    )),
+                });
+            }
+            return Err(BrregError {
+                typ: BrregErrorType::InternalServerError,
+                error: Some("Got unparsable 500 internal server error".to_string()),
+            });
+        }
+
+        _ => Err(BrregError {
+            typ: BrregErrorType::UnexpectedResponse,
+            error: Some(format!("Got status code {}", response.status().as_str()).to_string()),
+        }),
+    }
+}
+
+fn get_parent_org(client: &Client, org: &Organization) -> Result<Option<Organization>, BrregError> {
+    if org.overordnet_enhet.is_none() {
+        return Ok(None);
+    }
+
+    let parent_orgnr = org.overordnet_enhet.as_ref().unwrap();
+
+    let result = get_organization(client, &parent_orgnr, "enhet");
+    if result.is_ok() {
+        return Ok(Some(result.unwrap()));
+    }
+    return Err(result.unwrap_err());
+}
+
+fn get_child_orgs(client: &Client, parent_orgnr: &str) -> Result<Option<Underenheter>, BrregError> {
+    let http_result = client
+        .get("https://data.brreg.no/enhetsregisteret/api/underenheter")
+        .query(&[("overordnetEnhet", parent_orgnr)])
+        .send();
+
+    if http_result.is_err() {
+        return Err(BrregError {
+            typ: BrregErrorType::NetworkError,
+            error: Some(http_result.unwrap_err().to_string()),
+        });
+    }
+    let mut response = http_result.unwrap();
+
+    let mut body = String::new();
+    let read_result = response.read_to_string(&mut body);
+    if read_result.is_err() {
+        return Err(BrregError {
+            typ: BrregErrorType::NetworkError,
+            error: Some(read_result.unwrap_err().to_string()),
+        });
+    }
+
+    match response.status() {
+        StatusCode::OK => {
+            let json_parse_res = serde_json::from_str::<SearchResponse>(&body);
+
+            if json_parse_res.is_ok() {
+                return Ok(json_parse_res.unwrap()._embedded);
+            }
+            return Err(BrregError {
+                typ: BrregErrorType::JsonParseError,
+                error: Some(json_parse_res.unwrap_err().to_string()),
+            });
+        }
+
+        StatusCode::INTERNAL_SERVER_ERROR => {
+            let json_parse_res = serde_json::from_str::<BrregInternalServerError>(&body);
+            if json_parse_res.is_ok() {
+                let brreg_error = json_parse_res.unwrap();
+
+                return Err(BrregError {
+                    typ: BrregErrorType::InternalServerError,
+                    error: Some(format!(
+                        "{}: {} {}",
+                        brreg_error.trace, brreg_error.error, brreg_error.message
+                    )),
+                });
+            }
+            return Err(BrregError {
+                typ: BrregErrorType::JsonParseError,
+                error: Some(json_parse_res.unwrap_err().to_string()),
+            });
+        }
+
+        _ => Err(BrregError {
+            typ: BrregErrorType::UnexpectedResponse,
+            error: Some(format!("Got status code {}", response.status().as_str()).to_string()),
+        }),
+    }
+}
+
+fn print_address(address: Adresse) {
+    // ugh
+    for (pos, addresse) in address.adresse.iter().enumerate() {
+        if pos != 0 {
+            println!("{}  {}", pad_title(""), addresse);
+        } else {
+            println!("{}", addresse);
+        }
+    }
+
+    if address.adresse.len() != 0 {
+        print!("{}  ", pad_title(""));
+    }
+    if address.postnummer.is_some() && address.poststed.is_some() {
+        println!(
+            "{} {}",
+            address.postnummer.unwrap_or_default(),
+            address.poststed.unwrap_or_default(),
+        )
+    } else {
+        println!(
+            "{}{}",
+            address.postnummer.unwrap_or_default(),
+            address.poststed.unwrap_or_default(),
+        )
+    }
+}
+
+fn pad_title(title: &str) -> String {
+    return format!("{:width$}", title, width = MAX_TITLE_LENGTH);
+}
+
+fn get_norwegian_bool(input: bool) -> &'static str {
+    return if input { "Ja" } else { "Nei" };
+}
+
+fn print_org_info(
+    org_type: &str,
+    org: Organization,
+    maybe_parent_org: Option<Organization>,
+    maybe_child_orgs: Option<Underenheter>,
+) {
+    println!("********************** {} **********************", org_type);
+    println!("{}: {}", pad_title("Orgnummer"), org.organisasjonsnummer);
+    println!("{}: {}", pad_title("Navn"), org.navn);
+
+    if org.slettedato.is_some() {
+        println!("{}: {}", pad_title("Slettedato"), org.slettedato.unwrap());
+    }
+
+    if org.forretningsadresse.is_some() {
+        print!("{}: ", pad_title("Forretningsadresse"));
+        print_address(org.forretningsadresse.unwrap());
+    }
+    if org.postadresse.is_some() {
+        print!("{}: ", pad_title("Postadresse"));
+        print_address(org.postadresse.unwrap());
+    }
+
+    if org.hjemmeside.is_some() {
+        println!("{}: {}", pad_title("Hjemmeside"), org.hjemmeside.unwrap());
+    }
+
+    if org.registreringsdato_enhetsregisteret.is_some() {
+        println!(
+            "{}: {}",
+            pad_title("Registrert i Enhetsregisteret"),
+            org.registreringsdato_enhetsregisteret.unwrap()
+        );
+    }
+    if org.under_avvikling.is_some() {
+        println!(
+            "{}: {}",
+            pad_title("Under avvikling"),
+            get_norwegian_bool(org.under_avvikling.unwrap())
+        );
+    }
+    if org.under_tvangsavvikling_eller_tvangsopplosning.is_some() {
+        println!(
+            "{}: {}",
+            pad_title("Under tvangsavvikling eller tvangsoppløsning"),
+            get_norwegian_bool(org.under_tvangsavvikling_eller_tvangsopplosning.unwrap())
+        );
+    }
+
+    if maybe_parent_org.is_some() {
+        let parent_org = maybe_parent_org.unwrap();
+        println!(
+            "{}: {} - {}",
+            pad_title("Overordnet enhet"),
+            parent_org.organisasjonsnummer,
+            parent_org.navn
+        )
+    }
+
+    if maybe_child_orgs.is_some() {
+        let child_orgs = maybe_child_orgs.unwrap().underenheter;
+        if child_orgs.len() > 0 {
+            print!("{}:", pad_title("Underenheter (20 første)"));
+            for (pos, child_org) in child_orgs.iter().enumerate() {
+                if pos != 0 {
+                    println!(
+                        "{}  {} - {}",
+                        pad_title(""),
+                        child_org.organisasjonsnummer,
+                        child_org.navn
+                    );
+                } else {
+                    println!(" {} - {}", child_org.organisasjonsnummer, child_org.navn);
+                }
+            }
+        }
+    }
+}
+
+fn handle_main_error(error: BrregError) {
+    // Handles error for the "main" queries, where
+    // a 404 or 410 is to be expected, and should
+    // be communicated to the user.
+    match error.typ {
+        BrregErrorType::Gone => {
+            eprintln!("Denne enheten er fjernet fra brreg");
+        }
+
+        BrregErrorType::NotFound => {
+            eprintln!("Fant ikke denne enheten i brreg");
+        }
+
+        _ => handle_error(error),
+    }
+
+    std::process::exit(1);
+}
+
+fn handle_error(error: BrregError) {
+    // Handles error for extra queries (child/parent orgs)
+    // where it would be wrong to e.g. tell the user the
+    // org is missing on 404.
+    match error.typ {
+        BrregErrorType::NetworkError => {
+            eprintln!(
+                "Feil under kommunikasjon med brreg: {}",
+                error.error.unwrap()
+            );
+        }
+
+        BrregErrorType::UnexpectedResponse => {
+            eprintln!("Uventet svar fra brreg: {}", error.error.unwrap());
+        }
+
+        BrregErrorType::InternalServerError => {
+            eprintln!("Trøbbel i tårnet hos brreg: {}", error.error.unwrap());
+        }
+
+        BrregErrorType::JsonParseError => {
+            eprintln!(
+                "Klarte ikke lese svaret fra brreg: {}",
+                error.error.unwrap()
+            );
+        }
+
+        _ => {
+            panic!("Unexpected error type in handle_error: {:?}", error.typ)
+        }
+    }
+
+    std::process::exit(1);
+}
+
+fn handle_extra_call_to_brreg<T: std::fmt::Debug>(
+    result: Result<Option<T>, BrregError>,
+) -> Option<T> {
+    if result.is_ok() {
+        return result.unwrap();
+    }
+    handle_error(result.unwrap_err());
+    panic!("handle_error did not handle error")
+}
+
+fn main() {
+    let mut args = env::args();
+    let cmd_name = args.nth(0).unwrap();
+    let args: Vec<String> = args.collect();
+
+    // The user may run it with the org number
+    // spread over several arguments, like
+    // `brreg-cli 983 544 622`, or with spaces in it,
+    // so let's combine all arguments, strip spaces and
+    // then see if it's a nine digit number.
+    let combined_params = args.join("").replace(" ", "");
+
+    if combined_params == "--version" {
+        println!("Version: {} ", VERSION);
+        std::process::exit(0);
+    }
+
+    if combined_params.len() != 9 || combined_params.parse::<u32>().is_err() {
+        eprintln!("Usage: {} *orgnr*", cmd_name);
+        eprintln!("(orgnr must be nine numbers)");
+        std::process::exit(1);
+    }
+    let orgnr = combined_params;
+
+    let client = reqwest::blocking::Client::builder()
+        .timeout(Duration::from_secs(2))
+        .user_agent("https://github.com/magnuswatn/brreg-cli")
+        .use_rustls_tls()
+        .https_only(true)
+        .build()
+        .unwrap();
+
+    let main_result = get_organization(&client, &orgnr, "enhet");
+    if main_result.is_ok() {
+        let org = main_result.unwrap();
+        let parent_org = handle_extra_call_to_brreg(get_parent_org(&client, &org));
+        let child_orgs = handle_extra_call_to_brreg(get_child_orgs(&client, &orgnr));
+        print_org_info("Organisasjon", org, parent_org, child_orgs);
+    } else {
+        let err = main_result.unwrap_err();
+
+        if err.typ == BrregErrorType::NotFound {
+            // Maybe an underenhet
+            let child_result = get_organization(&client, &orgnr, "underenhet");
+            if child_result.is_ok() {
+                let org = child_result.unwrap();
+                let parent_org = handle_extra_call_to_brreg(get_parent_org(&client, &org));
+                print_org_info("Underenhet", org, parent_org, None);
+            } else {
+                handle_main_error(child_result.unwrap_err());
+            }
+        } else {
+            handle_main_error(err);
+        }
+    }
+}
